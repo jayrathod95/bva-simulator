d131b4709835f791ea89465e02559fb3d68ed2d2
Jay Rathod <jay_rathod@live.com>
2019-11-08 00:47:43 +0530

    original version

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,119 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/bva',function (req, res) {
+
+  var clientId = '3MVG9qwrtt_SGpCvjWgjr6iTlzasfEXKEX4CpJH697tmvMKY_REcAosImBkFzgWdcPlPrh2WpNZt6sT51CMzi';
+  var clientSecret = '6B910DEF3E10F6CCE4D976A6E9FA94BFA5182C2048B0EC16EAAE51BCA562B3E5';
+  var username = 'integration.user@abbvie.com.pap.rhfd';
+  var password = 'newuser@12345\n';
+  //authenticate with sf
+  var data = `grant_type=password&client_id=${clientId}&client_secret=${clientSecret}&username=${username}&password=${password}`;
+  axios.post('https://test.salesforce.com/services/oauth2/token',data).then(response => {
+    console.info(response.data);
+    //console.info(response.data.access_token);
+
+    //call bva-inbound
+    var data = {
+      "Patient": {
+        "transactionType": "BVS",
+        "transactionMessage": "Eligibility and Benefits Found",
+        "subscriber": "PPM",
+        "puslisherRequestId": "23980173579473615274",
+        "publisher": "BVA",
+        "dateSent": "2018-11-08T09:07:18.684Z",
+        "publisherId": "6404140854",
+        "patientDiagnosisAndTreatments": [{
+          "publisherId": "1442_drug",
+          "ndc": "00074-3799-02",
+          "drugName": "RINVOQ"
+        }],
+        "memberPlans": [{
+          "publisherId": "73732887",
+          "rxPCN": "73732887",
+          "rxGroupNumber": "73732887",
+          "rxBIN": "73732887",
+          "relationshipToCardholder": "Cardholder",
+          "planType": "Commercial",
+          "planSubType": "Employer",
+          "insuranceType": "Pharmacy Plan",
+          "insuranceRank": 1,
+          "insurancePlanPhone": 9878786511,
+          "insuranceName": "Rose R",
+          "endDate": "2019-07-28",
+          "employerName": "AbbVie",
+          "effectiveDate": "2018-07-10",
+          "disadvantagePlan": true,
+          "cardholderOrPolicyID": "MID809",
+          "cardHolderName": "Rose R",
+          "disadvantageAccessReason": "Affordability"
+        }		],
+        "priorAuthorizations": [{
+          "publisherId": "1866899",
+          "statusforVariableParagraph": "Approved with Expiration",
+          "statusDate": "2019-03-29",
+          "status": "PA Approved",
+          "planPhoneNumber": "9898989892",
+          "planName": "PA for Bridge 4",
+          "planFaxNumber": "9898989898",
+          "patientInsuranceId": "73732887",
+          "formURL": "https://master-api.integration.covermymeds.com/forms/pdf/thumbs/270/pseudo_4part_28520.jpg",
+          "formName": "pseudo_4part"
+        }],
+        "memberBenefits": [{
+          "publisherId": "28440176",
+          "specialtyPharmacyOptions": "One Mandated Pharmacy 10",
+          "publisherMemberPlanId": "73732887",
+          "priorAuthorizationRequired": "Yes",
+          "pharmacyMedicalAnnualOOPMetToDate": 1250.0,
+          "pharmacyMedicalAnnualOOP": 13030.0,
+          "pharmacyCopay": 1210.0,
+          "pharmacyAnnualDeductibleMetToDate": 1451.0,
+          "pharmacyAnnualDeductible": 1510.0,
+          "patientResponsibility": "Copayment",
+          "patientPharmacies": [{
+            "publisherId": "49517633",
+            "transactionType": "BVS",
+            "publisherMemberPlanId": "73732887",
+            "pharmacyZipCode": "70185",
+            "pharmacyState": "IL",
+            "pharmacyPhone": "8898985419",
+            "pharmacyName": "June Patient Pharmacy 3",
+            "pharmacyFax": "4490105419",
+            "pharmacyCity": "Waukegan",
+            "pharmacyAddressLine2": "Suite 01",
+            "pharmacyAddressLine1": "2 lake street"
+          }],
+          "coverageStatus": "Not Covered",
+          "benefitsSource": "New"
+        }]
+      }
+    };
+
+    axios.put(`${response.data.instance_url}/services/apexrest/dataexchange/v1/bvs`,data,{headers: {'Content-Type':'application/json','Authorization':`Bearer ${response.data.access_token}`}}).then(response => {
+      console.log(response.data);
+    },reason => {
+      console.error(reason);
+    })
+
+  },reason => {
+    console.error(reason);
+  });
+  res.send(req.body);
+
+/*  axios.post('https://reqres.in/api/users',{
+    "name": "morpheus",
+    "job": "leader"
+  }).then(function (value) {
+    req.send(value);
+  })*/
+
 });
 
 module.exports = router;
