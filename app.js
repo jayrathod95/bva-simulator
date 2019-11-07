f91ed3e52b0fc21e2ef01a0bbc6588ac747ee17b
Jay Rathod <jay_rathod@live.com>
2019-11-08 00:46:49 +0530

    aaa

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(bodyParser.json());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 module.exports = app;
